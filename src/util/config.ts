58da8ea31f429f52c0567ffe22445ae295a435ec
Samjay Awinteck <basampana@st.ug.edu.gh>
2026-03-16 20:49:03 +0000

    update ci cd

--- src/util/config.ts
+++ src/util/config.ts
@@ -2,11 +2,11 @@ import * as dotenv from 'dotenv';
 import path from 'path';
 
 
-const envFile = process.env.NODE_ENV === 'production' ? '.env.prod' : '.env.dev';
+const envFile = process.env.NODE_ENV === 'production' ? '.env.gateway' : '.env.dev';
 dotenv.config({ path: path.resolve(__dirname, `../../${envFile}`) });
 
 export const config = {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 9000,
     authServiceUrl: process.env.AUTH_SERVICE_URL || '',
     inventoryServiceUrl: process.env.INVENTORY_SERVICE_URL || '',
     invoiceServiceUrl: process.env.INVOICE_SERVICE_URL || '',
